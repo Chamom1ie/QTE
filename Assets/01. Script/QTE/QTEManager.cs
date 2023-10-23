63d1fb064911c076b8e72b7a972bdffca80a9b7c
Chamom1ie <GitHubmile@gmail.com>
2023-10-23 10:58:40 +0900

    FindActionMap
    hahahahahahahahahahahahah

--- Assets/01. Script/QTE/QTEManager.cs
+++ Assets/01. Script/QTE/QTEManager.cs
@@ -6,15 +6,31 @@ using UnityEngine.InputSystem;
 public class QTEManager : MonoBehaviour
 {
     [SerializeField] private InputReader _inputReader;
-
+    InputActionMap playerMap;
+    InputActionMap QTEMap;
     private void Awake()
     {
         _inputReader.ActionMapControl += ActionMapChanger;
+        playerMap = _inputReader.GetControl().FindAction("Movement")?.actionMap;
+        QTEMap = _inputReader.GetControl().FindAction("QTEInput")?.actionMap;
+
+        Debug.Log(playerMap);
+        Debug.Log(QTEMap);
     }
 
     void ActionMapChanger()
     {
-        Debug.Log(_inputReader.GetControl().FindAction("Player")?.actionMap);
+        if (playerMap.enabled)
+        {
+            QTEMap.Enable();
+            playerMap.Disable();
+            Debug.Log("플레이어뒤짐");
+        }
+        else if (QTEMap.enabled)
+        {
+            playerMap.Enable();
+            QTEMap.Disable();
+        }
     }
 }
     
